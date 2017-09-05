f1c27937121575b65bbcfe704ba295f7a3358703
Diego Leite <diegojleite@gmail.com>
2017-09-05 14:49:09 -0300

    Styles for footer and fixes for responsive navbar

--- matrix/js/theme.js
+++ matrix/js/theme.js
@@ -1,8 +1,3 @@
-/*
- * This JavaScript doesn't do anything. The file exists just to demonstrate
- * including static assets from the HTML in themes.
- */
-
 $(function () {
   $('.dropdown-submenu a.submenu').on("mouseover", function (e) {
     $('.dropdown-submenu > ul').hide();
@@ -11,29 +6,22 @@ $(function () {
     e.preventDefault();
   });
 
-  console.log('weg')
-
   let latchY = 0
   let caught = false;
   let t;
 
   $('html').on('mousewheel', function (e) {
     let pos = document.getElementsByClassName('toc')[0].getClientRects()[0];
-    console.log('pos', ':', pos);
 
     if (!caught && window.scrollY > latchY && pos.top < 100) {
       // find roughly where the top of this thing is
       latchY = window.scrollY;
-      console.log('latch', ':', latchY);
       $('.toc').addClass('float');
       caught = true
     } else if (caught && window.scrollY < latchY) {
       latchY = 0;
       caught = false;
-      console.log('unlatch', ':', latchY);
       $('.toc').removeClass('float');
     }
-
-
-  })
+  });
 });
