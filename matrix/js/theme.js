0b13fbd7a99c9cbbd99724949686c9675759bc98
Hermitter <chacin.carlos9798@gmail.com>
2019-06-12 19:21:56 -0400

    Added a basic copy button replacement

--- matrix/js/theme.js
+++ matrix/js/theme.js
@@ -1,4 +1,31 @@
 $(function () {
+  /* Clipboard integration */
+  if (ClipboardJS.isSupported()){
+    // get all code elements
+    var allCodeBlocksElements = $( "div.codehilite pre" );
+
+    // For each element, do the following steps
+    allCodeBlocksElements.each(function(ii) {
+    // define a unique id for this element and add it
+    var currentId = "codeblock" + (ii + 1);
+    $(this).attr('id', currentId);
+
+    // create a button that's configured for clipboard.js
+    // point it to the text that's in this code block
+    // add the button just after the text in the code block w/ jquery
+    var clipButton = '<a class="btn copybtn" data-clipboard-target="#' + currentId + '"><img src="https://clipboardjs.com/assets/images/clippy.svg" width="13" alt="Copy to clipboard"></a>';
+        $(this).after(clipButton);
+    });
+
+    // tell clipboard.js to look for clicks that match this query
+    var clipboard = new ClipboardJS('.btn');
+
+    // deselect 
+    clipboard.on('success', (event)=> {
+      event.clearSelection();
+    });
+  }
+
   //Drop Down Submenu Menu\\
   $('.dropdown-submenu a.submenu').on("mouseover", function (e) {
     $('.dropdown-submenu > ul').hide();
