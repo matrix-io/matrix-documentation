d0d2cd3a467ea95e4ef5ec7a99d7332b99c4b3e4
Hermitter <chacin.carlos9798@gmail.com>
2019-06-14 16:14:34 -0400

    Improved copy notify

--- matrix/js/theme.js
+++ matrix/js/theme.js
@@ -1,7 +1,5 @@
 $(function () {
   //Clipboard.js integration\\
-
-  // find all code block elements
   if (ClipboardJS.isSupported()){
     var allCodeBlocksElements = $( "div.codehilite pre" );
 
@@ -12,26 +10,37 @@ $(function () {
     $(this).attr('id', currentId);
 
     // create the button just after the text in the code block
-    var clipButton = '<a class="btn copybtn" data-clipboard-target="#'+currentId+'"><img src="https://clipboardjs.com/assets/images/clippy.svg" draggable="false" width="13" alt="Copy to clipboard"></a>';
+    var clipButton = '<a class="btn copybtn" data-clipboard-target="#'+currentId+'" allow-copy-notify="true"><img src="'+base_url+'/img/clippy.svg" draggable="false" width="13" alt="Copy to clipboard"></a>';
         $(this).after(clipButton);
     });
 
-    // initialize each clipboard button
-    var clipboard = new ClipboardJS('.btn');
+    var clipboard = new ClipboardJS(".btn");
 
-    // cleanup & Copy Notification
-    clipboard.on('success', (event)=> {
-      // show copy notification
+    clipboard.on("success", (event)=> {
+      // find button html element
       var buttonIndex = event.trigger.dataset.clipboardTarget.replace(/[^0-9]/g,'')-1;
       var copyButton = $(".copybtn:eq("+buttonIndex+")");
-      copyButton.tooltip({trigger: 'click'});
-      copyButton.tooltip('hide').attr('data-original-title', "copied!").tooltip('show');
 
-      // delay to remove copy notification
-      setTimeout(function() {
-        copyButton.tooltip('hide');
-        copyButton.css("background-color","white");
-      }, 1000);
+      // button click animation
+      copyButton.addClass("copy-success");
+      setTimeout(function(){copyButton.removeClass("copy-success");},100);
+
+      // show copy notification
+      if(copyButton.attr("allow-copy-notify") === "true"){
+        copyButton.tooltip({trigger: "click"});
+        copyButton.attr("allow-copy-notify", false);
+        copyButton.tooltip("hide").attr("data-original-title", "copied!").tooltip("show");
+        
+        // remove copy notification
+        setTimeout(function() {
+          copyButton.tooltip("hide");
+          copyButton.css("background-color","white");
+          copyButton.attr("allow-copy-notify", true);
+        }, 1000);
+      }
+      
+      // prevent copy notification spam
+      copyButton.tooltip().off();
 
       // prevent code block from being highlighted
       event.clearSelection();
